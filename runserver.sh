6c30c40ccebaaffb22f52b7ed1dcc200aac0b502
SoftProDev <softprodev15@gmail.com>
2008-09-28 16:02:34 +0200

    Description files.

--- runserver.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/bash
-source /usr/local/bin/use-django
-use-django r9067
-if [[ $1 != "" ]]; then
-    python manage.py 7483
-else
-    screen python manage.py runserver `hostname`:7483
-fi
-
